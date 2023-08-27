952932f17cec058b6e04033f4b5d721a8cc86761
davi41229 <davi41229@outlook.com>
2023-08-27 03:56:03 -0300

    update-main.py-rodar-no-server-render

--- main.py
+++ main.py
@@ -7,20 +7,25 @@ import os
 
 
 
-
-
+"""
+para rodar em desenvolvimento de teste em maquina
 
 if __name__ == '__main__':
     if not os.path.exists('storage.db'):
         db.create_all()
     app.run(debug=True,host="0.0.0.0", port=7540)
 
-
 """
-para fazer o deploy e iniciar no servidor do render
+
+
+
+
+# para fazer o deploy e iniciar no servidor do render
 
 if __name__ == '__main__':
+    if not os.path.exists('storage.db'):
+        db.create_all()
     port = int(os.getenv("PORT"), "5000")
     app.run(host="0.0.0.0", port=port)
 
-"""
+
